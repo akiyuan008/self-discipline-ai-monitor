9af7df1ce57761e878cf78798ad120ad94fd242e
akiyuan008 <akisource@outlook.com>
2026-08-03 23:15:59 +0800

    feat(main): App进入后台时自动备份数据

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { App as CapApp } from '@capacitor/app'
 import './styles/index.css'
 import App from '@/App'
+import { autoBackup } from '@/lib/backup'
+
+// 自动备份：App 进入后台时触发
+CapApp.addListener('pause', () => {
+  autoBackup().catch(() => {})
+})
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
